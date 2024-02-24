7a7f163e91aaf4e3a926c914dd33048dfab19361
silencer <general@enkit.ru>
2024-02-25 01:09:19 +0700

    fixed port 80->443

--- src/pages/Chats.jsx
+++ src/pages/Chats.jsx
@@ -5,7 +5,7 @@ import PostService from "../API/PostService";
 import Chat from "../components/UI/Chat/Chat";
 
 //const server_path = 'ws://192.168.0.12:8000/' //debug
-const server_path = 'wss://messenger.enkit.ru:80/api_ws/' //production
+const server_path = 'wss://messenger.enkit.ru:443/api_ws/' //production
 // const server_path = 'ws://192.168.0.17:8000/' //production
 
 
