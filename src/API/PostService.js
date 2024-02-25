3ef963a6aef81e189a973c8d4dd14d0b46e8d31d
silencer <general@enkit.ru>
2024-02-25 12:02:44 +0700

    Fixed bug

--- src/API/PostService.js
+++ src/API/PostService.js
@@ -7,17 +7,6 @@ const server_path = 'https://messenger.enkit.ru:443/api/' //debug
 // const server_path = 'http://192.168.0.17:8000/' //production
 
 
-const getSHA256Hash = async (input) => {
-  const textAsBuffer = new TextEncoder().encode(input);
-  const hashBuffer = await window.crypto.subtle.digest("SHA-256", textAsBuffer);
-  const hashArray = Array.from(new Uint8Array(hashBuffer));
-  const hash = hashArray
-    .map((item) => item.toString(16).padStart(2, "0"))
-    .join("");
-  return hash;
-};
-
-
 export default class PostService {
 
 
